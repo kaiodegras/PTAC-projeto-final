c2e30c5d61e2bd8abe505fbe2cf91a54f56b9e8d
kaio <kaioa9851@gmail.com>
2023-11-24 18:33:30 -0300

    Adicionei o navbar
    Adicionei e alterei elementos dentro do navbar

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,10 @@
+import './home.css'
 export default function Home(){
+
     return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
+        
+        <h1></h1>
+
+   
      );
 }
---
+++ src/props.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+function cards({ListaVideos}) {
+    return(
+        
+        ListaVideos.map((video) =>
+        <div className="card">
+            <iframe
+                width="853"
+                height="480"
+            ></iframe>
+          
+        </div>
+        
+        )
+    )
+}
+
+<div className="conatiner">
+   
+</div>
